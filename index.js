e69ba83ef2b5745dd3902599dc1788b2fcf801a5
MetaMsa <mserhataslan@hotmail.com>
2024-03-04 02:48:54 +0300

    change

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ const express=require("express");
 const app=express();
 
 app.set("view engine", "ejs");
+app.use(express.static('public'));
 
 app.use("/", function(req, res) {
     res.render("index");
 });
-
-app.use(express.static("public"));
 
 app.listen(process.env.PORT || 5000);
