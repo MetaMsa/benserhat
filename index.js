94bff0d3247fdac8a27dc8851d1cc2c3dda8714f
MetaMsa <mserhataslan@hotmail.com>
2024-03-04 15:15:42 +0300

    changed

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app=express();
 app.set("view engine", "ejs");
 app.use(express.static('public'));
 
+app.use("/gpdr", function(req, res) {
+    res.render("gpdr.ejs");
+});
+
 app.use("/", function(req, res) {
     res.render("index");
 });
