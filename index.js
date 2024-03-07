2915f544453adbaf66c4891074166bfaea51f00f
MetaMsa <mserhataslan@hotmail.com>
2024-03-08 02:21:40 +0300

    change

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const express=require("express");
 const app=express();
+var getJSON = require("get-json");
 
 app.set("view engine", "ejs");
 app.use(express.static('public'));
+
+getJSON('https://youtube.googleapis.com/youtube/v3/channels?part=statistics&id=UCCaXdWt0SGQnVHxpGmpz_EQ&key=AIzaSyAfwVlJJBg1eegX0V8aefJ5dM9Gj6o5fXQ', function(error, response){
+    return subs=response.items[0].statistics.subscriberCount;
+});
 
 app.use("/gdpr", function(req, res) {
     res.render("gdpr.ejs");
 });
 
 app.use("/", function(req, res) {
-    res.render("index");
+    res.render("index",{
+        sub: subs
+    });
 });
 
 app.listen(process.env.PORT || 5000);
