bf69b7a94beb775d2637536600cad9b5433948e7
MetaMsa <mserhataslan@hotmail.com>
2024-03-08 00:38:58 +0300

    change

--- public/js/js.js
+++ public/js/js.js
@@ -1,9 +1,3 @@
-window.addEventListener('load', loadd);
-
-function loadd() {
-    document.getElementById('loading').style.display = 'none';
-}
-
 var karsilama="MERHABA";
 var i=0;
 const myinterval = setInterval(() => {document.getElementById("merhaba").innerHTML+=karsilama[i];i++;if(i==8) {i=0;document.getElementById("merhaba").innerHTML=" ";}}, 1000);
