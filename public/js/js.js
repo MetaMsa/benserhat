10d8b3ac6a56c9d538e4580c3ebef8e062358e29
MetaMsa <mserhataslan@hotmail.com>
2024-03-09 20:47:20 +0300

    responsive

--- public/js/js.js
+++ public/js/js.js
@@ -2,6 +2,6 @@ window.onload = () => {
     document.querySelector(".loader").style.display = "none";
 }
 
-var karsilama="MERHABA";
+var karsilama="ERHABA";
 var i=0;
-const myinterval = setInterval(() => {document.getElementById("merhaba").innerHTML+=karsilama[i];i++;if(i==8) {i=0;document.getElementById("merhaba").innerHTML=" ";}}, 1000);
+const myinterval = setInterval(() => {document.getElementById("merhaba").innerHTML+=karsilama[i];i++;if(i==7) {i=0;document.getElementById("merhaba").innerHTML="M";}}, 1000);
