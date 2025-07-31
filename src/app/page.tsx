6d5248e5f10d397b3cec2c2980d38ff32c51ecca
MetaMsa <mserhataslan@hotmail.com>
2025-07-31 21:52:40 +0300

    main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
       .then((data) => (setData(data.items[0].statistics.subscriberCount)));
   }, []);
   return (
-    <div className="grid grid-cols-1 sm:grid-col-2 md:grid-cols-3 m-10 p-10 bg-gray-900 rounded-xl">
+    <div className="grid grid-cols-1 sm:grid-col-2 md:grid-cols-3 mx-10 my-20 p-10 bg-gray-900 rounded-xl">
       <div>
         <Image
           className="rounded-xl mx-auto"
