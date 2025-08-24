4e3218d827f82c2757920c5f5e76f070fd2867fc
MetaMsa <mserhataslan@hotmail.com>
2025-08-24 16:45:43 +0300

    blog

--- src/app/api/saveBlog/route.ts
+++ src/app/api/saveBlog/route.ts
@@ -1,12 +1,22 @@
 import { redirect } from "next/navigation";
 import { _Blog } from "@/Model/Blog";
+import { _Comments } from "@/Model/Blog";
 import { connectDb } from "@/lib/connectDb";
+import { NewsMailSend } from "@/lib/mailSend";
 
 export async function POST(req: Request) {
   const blogData = await req.formData();
 
   await connectDb();
 
+  const emails = await _Comments.find();
+  const emailList = emails.map(comment => ({
+  email: comment.email,
+  name: comment.name || "Kullanıcı"
+}));
+
+  NewsMailSend(emailList)
+  
   if ((await _Blog.findOne({ title: blogData.get("title") })) == null) {
     const blogText = new _Blog({
       title: blogData.get("title"),
--- src/lib/mailSend.ts
+++ src/lib/mailSend.ts
@@ -1,6 +1,43 @@
 import { MailerSend, EmailParams, Sender, Recipient } from "mailersend";
 
-export async function ReplyMailSend(_replyUserName, _reply, _clientMail, _clientUserName) {
+export async function NewsMailSend(
+  _clients: { email: string; name: string }[]
+) {
+  const mailerSend = new MailerSend({
+    apiKey: process.env.MAILERSEND_API_KEY || "",
+  });
+
+  const sentFrom = new Sender("MS_Agq6XW@benserhat.live", "benserhat");
+
+  const uniqueClients = Array.from(
+    new Map(_clients.map((c) => [c.email, c])).values()
+  );
+
+  const recipients = uniqueClients.map((c) => new Recipient(c.email, c.name));
+
+  const personalization = _clients.map((client) => ({
+    email: client.email,
+    data: {
+      name: client.name,
+    },
+  }));
+
+  const emailParams = new EmailParams()
+    .setFrom(sentFrom)
+    .setTo(recipients)
+    .setSubject("Blog sitemizde bir güncelleme var")
+    .setTemplateId("z86org8o95n4ew13")
+    .setPersonalization(personalization);
+
+  await mailerSend.email.send(emailParams);
+}
+
+export async function ReplyMailSend(
+  _replyUserName,
+  _reply,
+  _clientMail,
+  _clientUserName
+) {
   const mailerSend = new MailerSend({
     apiKey: process.env.MAILERSEND_API_KEY || "",
   });
