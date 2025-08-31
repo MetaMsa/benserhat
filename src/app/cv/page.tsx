76151a9695cf2690cf7011526f22364ece671db1
MetaMsa <mserhataslan@hotmail.com>
2025-08-31 16:01:36 +0300

    blog

---
+++ src/app/cv/page.tsx
@@ -0,0 +1,11 @@
+export default function Cv()
+{
+    return(
+        <div className="">
+            <iframe
+        src="/public/cv.pdf"
+        style={{ width: "100%", height: "100%", border: "none" }}
+      />
+        </div>
+    )
+}
