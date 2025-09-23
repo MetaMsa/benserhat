dc8849620b248cb6cb9e27bcba30d8f842ba40f7
MetaMsa <mserhataslan@hotmail.com>
2025-09-23 20:09:09 +0300

    main

---
+++ src/app/api/docs/route.ts
@@ -0,0 +1,7 @@
+import swaggerDocument from "../../../../swagger.json";
+import { NextResponse } from "next/server";
+
+export async function GET()
+{
+    return NextResponse.json(swaggerDocument);
+}
