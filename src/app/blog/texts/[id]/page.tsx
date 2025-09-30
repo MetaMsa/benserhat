e2fbc14057f1a137ea3f9e6d28c53df08430dff7
MetaMsa <mserhataslan@hotmail.com>
2025-09-30 21:08:39 +0300

    comment

--- src/app/blog/texts/[id]/page.tsx
+++ src/app/blog/texts/[id]/page.tsx
@@ -82,7 +82,7 @@ export default async function BlogText({ params }) {
       <div
         className="break-words text-xs sm:text-sm p-5 mx-auto my-5 text-left bg-gray-900 rounded-xl border"
         dangerouslySetInnerHTML={{
-          __html: text.content.replace(/<img(.*?)>/g, '<img loading="lazy"$1 alt="Blog Image">'),
+          __html: text.content.replace(/<img(.*?)>/g, '<img loading="lazy"$1 alt="Blog Image">'), // lazy load ve alt text
         }}
       ></div>
       <div className="mt-auto">
