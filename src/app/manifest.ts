6eaa063b1892677f50f061d7f2c049f7745a94c6
MetaMsa <mserhataslan@hotmail.com>
2026-02-05 18:15:53 +0300

    updated manifest.json

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -12,6 +12,8 @@ export default function manifest(): MetadataRoute.Manifest {
         icons: [
             {
                 src: "/favicon.ico",
+                sizes: "any",
+                type: "image/x-icon",
             },
         ],
     };
